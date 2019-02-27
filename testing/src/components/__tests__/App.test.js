4d79f790eedffd00f541d84eca56b61dc5d025d6
Kenta Kodashima <kentakodashima@gmail.com>
2019-02-26 16:05:41 -0800

    Add Enzyme configuration.

--- testing/src/components/__tests__/App.test.js
+++ testing/src/components/__tests__/App.test.js
@@ -1,18 +1,36 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { shallow } from 'enzyme'
 import App from '../App'
+import CommentBox from "../CommentBox";
+import CommentList from "../CommentList";
 
 it('shows a comment box', () => {
-  // Create fake div inside of JSDOM in Jest
-  const div = document.createElement('div')
+  const component = shallow(<App />)
 
-  // Take the instance of App component and stick to 'div'
-  ReactDOM.render(<App />, div)
-
-  // Looks inside the div
-  // Check to see if the CommentBox is in there
-  expect(div.innerHTML).toContain('Comment Box')
-
-  // Clear the element away when the test ends
-  ReactDOM.unmountComponentAtNode(div)
+  expect(component.find(CommentBox).length).toEqual(1)
 })
+
+it('shows a comment list', () => {
+  const component = shallow(<App />)
+
+  expect(component.find(CommentList).length).toEqual(1)
+})
+
+// import ReactDOM from 'react-dom'
+// import CommentBox from '../CommentBox';
+
+// it('shows a comment box', () => {
+//   // Create fake div inside of JSDOM in Jest
+//   const div = document.createElement('div')
+
+//   // Take the instance of App component and stick to 'div'
+//   ReactDOM.render(<App />, div)
+
+//   // Looks inside the div
+//   // Check to see if the CommentBox is in there
+//   // Bad practice because it counts on the contents of the component
+//   // expect(div.innerHTML).toContain('Comment Box')
+
+//   // Clear the element away when the test ends
+//   ReactDOM.unmountComponentAtNode(div)
+// })
